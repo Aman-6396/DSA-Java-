7d91561d47343a976d4606983f76b6e0f5f727dd
Aman Mishra <mishraaman7417@gmail.com>
2026-04-25 23:46:40 +0530

    Strings

---
+++ Strings/Furthest_Point_From_Origin_Leetcode_2833.java
@@ -0,0 +1,22 @@
+package Leetcode_004_April_2026;
+
+public class Furthest_Point_From_Origin_Leetcode_2833 {
+    class Solution {
+        public int furthestDistanceFromOrigin(String moves) {
+            int leftCount = 0;
+            int rightCount = 0;
+            int blankCount = 0;
+            for (int i = 0; i < moves.length(); i++) {
+                char ch = moves.charAt(i);
+                if (ch == 'R') {
+                    rightCount++;
+                } else if (ch == 'L') {
+                    leftCount++;
+                } else {
+                    blankCount++;
+                }
+            }
+            return Math.abs(rightCount - leftCount) + blankCount;
+        }
+    }
+}
