8b85d244329d6c6dc354e751be206040b4b3318d
Aman Mishra <mishraaman7417@gmail.com>
2026-05-09 11:05:30 +0530

    Arrays

---
+++ Arrays/Minimum_Absolute_Difference_Between_Two_Values_Leetcode_3880.java
@@ -0,0 +1,28 @@
+package Leetcode_004_April_2026;
+
+public class Minimum_Absolute_Difference_Between_Two_Values_Leetcode_3880 {
+    class Solution {
+        public int minAbsoluteDifference(int[] nums) {
+            int ans = 1000;
+            int one = -1;
+            int two = -1;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 1) {
+                    one = i;
+                } else if (nums[i] == 2) {
+                    two = i;
+                }
+
+                if (one != -1 && two != -1) {
+                    ans = Math.min(ans, Math.abs(one - two));
+                }
+            }
+
+            if (ans == 1000) {
+                return -1;
+            }
+
+            return ans;
+        }
+    }
+}
