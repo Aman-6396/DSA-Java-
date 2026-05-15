29bf9a5c2f7ccde76d8e170fb4952f0e42bbd3b2
Aman Mishra <mishraaman7417@gmail.com>
2026-05-15 22:49:52 +0530

    Math

---
+++ Math/Global_and_Local_Inversions_Leetcode_775.java
@@ -0,0 +1,14 @@
+package Leetcode_005_May_2026;
+
+public class Global_and_Local_Inversions_Leetcode_775 {
+    class Solution {
+        public boolean isIdealPermutation(int[] nums) {
+            for (int i = 0; i < nums.length; i++) {
+                if (Math.abs(nums[i] - i) > 1) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
