86507bad4487316a682da04adf0d239e151b8669
Aman Mishra <mishraaman7417@gmail.com>
2026-07-07 23:39:31 +0530

    Binary Search

---
+++ Binary Search/Median_of_Two_Sorted_Arrays_Leetcode_4.java
@@ -0,0 +1,47 @@
+public class Median_of_Two_Sorted_Arrays_Leetcode_4 {
+    class Solution {
+        public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+            int n1 = nums1.length;
+            int n2 = nums2.length;
+            if (n1 > n2) {
+                return findMedianSortedArrays(nums2, nums1); // means we always check smaller array
+            }
+            int left = 0;
+            int right = n1;
+            int partition = (n1 + n2 + 1) / 2;
+            while (left <= right) {
+                int mid1 = left + (right - left) / 2;
+                int mid2 = partition - mid1;
+                int l1 = Integer.MIN_VALUE;
+                int l2 = Integer.MIN_VALUE;
+                int r1 = Integer.MAX_VALUE;
+                int r2 = Integer.MAX_VALUE;
+                if (mid1 < n1) {
+                    r1 = nums1[mid1];
+                }
+                if (mid2 < n2) {
+                    r2 = nums2[mid2];
+                }
+                if (mid1 - 1 >= 0) {
+                    l1 = nums1[mid1 - 1];
+                }
+                if (mid2 - 1 >= 0) {
+                    l2 = nums2[mid2 - 1];
+                }
+
+                if (l1 > r2) {
+                    right = mid1 - 1;
+                } else if (l2 > r1) {
+                    left = mid1 + 1;
+                } else if (l1 <= r2 && l2 <= r1) {
+                    if ((n1 + n2) % 2 == 0) { // for even length
+                        return (Math.max(l1, l2) + Math.min(r1, r2)) / 2.0;
+                    } else { // for odd length
+                        return Math.max(l1, l2);
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
